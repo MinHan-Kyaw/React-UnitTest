27e84498af25e12e2a11f94e7362ba9c4c0b7cc8
Min Han Kyaw <minhankyaw.mh@gmail.com>
2024-07-29 16:28:02 +0700

    Working With Mocks

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -4,6 +4,10 @@ import Async from "./Async";
 describe("Async component", () => {
   test("renders post if request succeeds", async () => {
     // Arrange
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "p1", title: "First Post" }],
+    });
     render(<Async />);
 
     // Act
